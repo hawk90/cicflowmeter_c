7d842a9ce418fc70c0e93ac8022360a13a135b35
hawk90 <hawking90a@gmail.com>
2020-07-22 03:29:07 +0000

    리택토링: suricata -> cicflowmeter

---
+++ src/cicflowmeter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <pcap.h>
+#include "common/cicflowmeter_common.h"
+
+int main(int argc, char *argv[])
+{
+	printf("\n\n");
+	printf("==================================\n");
+	printf("=          CICFLOWMETER          =\n");
+	printf("==================================\n");
+	printf("\n\n");
+
+	return 0;
+}
