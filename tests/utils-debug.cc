b441ccef167c1d8575ea7af1cb84a9eeb85fc6e0
hawk90 <hawking90a@gmail.com>
2020-08-02 18:55:02 +0900

    Update: profiling

--- tests/utils-debug.cc
+++
@@ -1,7 +0,0 @@
-#include <gtest/gtest.h>
-#include "utils-debug.h"
-
-TEST(debugger_test, log_level)
-{
-	EXPECT_EQ(LOG_NOTSET, -1);
-}
--- tests/utils-debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CICFLOWMETER_TEST_UTILS_DEBUG_
-#define _CICFLOWMETER_TEST_UTILS_DEBUG_
-
-extern "C" {
-	#include "cicflowmeter-c/utils/debug.h"
-}
-
-#endif
