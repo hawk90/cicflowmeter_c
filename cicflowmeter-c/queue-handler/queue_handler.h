51d8ddf3f84881e59844f317512984cb65af1717
hawk90 <hawing90a@gmail.com>
2020-11-23 06:14:38 +0000

    Update: files

--- cicflowmeter-c/queue-handler/queue_handler.h
+++ cicflowmeter-c/queue-handler/queue_handler.h
@@ -5,6 +5,33 @@
 extern "C" {
 #endif
 
+enum {
+    TMQH_NOT_SET,
+    TMQH_SIMPLE,
+    TMQH_PACKETPOOL,
+    TMQH_FLOW,
+
+    TMQH_SIZE,
+};
+
+typedef struct Tmqh_ {
+    const char *name;
+    Packet *(*InHandler)(ThreadVars *);
+    void (*InShutdownHandler)(ThreadVars *);
+    void (*OutHandler)(ThreadVars *, Packet *);
+    void *(*OutHandlerCtxSetup)(const char *);
+    void (*OutHandlerCtxFree)(void *);
+    void (*RegisterTests)(void);
+} Tmqh;
+
+extern Tmqh tmqh_table[TMQH_SIZE];
+
+void TmqhSetup(void);
+void TmqhCleanup(void);
+int TmqhNameToID(const char *name);
+Tmqh *TmqhGetQueueHandlerByName(const char *name);
+Tmqh *TmqhGetQueueHandlerByID(const int id);
+
 #ifdef __cplusplus
 }
 #endif
